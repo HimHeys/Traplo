5018fae84772ebf1d54bcf15dd807b08230a2811
KimSangHyun <kshy1019@naver.com>
2021-08-09 17:17:05 +0900

    로그인,회원가입 버튼

--- Traplo/Traplo/ViewController.swift
+++ Traplo/Traplo/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnSignIn: UIButton!
+    @IBOutlet weak var btnSignUp: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        btnSignIn.layer.borderColor = UIColor.white.cgColor
+        btnSignIn.layer.borderWidth = 2
+        btnSignIn.layer.cornerRadius = 10
+
+        btnSignUp.layer.borderColor = UIColor.white.cgColor
+        btnSignUp.layer.borderWidth = 2
+        btnSignUp.layer.cornerRadius = 10
+
     }
 
 

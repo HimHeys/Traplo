f63fa831a94f9b6bce273719b363b6167052d3ad
KimSangHyun <kshy1019@naver.com>
2021-08-12 00:51:27 +0900

    mainUI 수정 2
    구글,네이버 로그인 추가 및 애니메이션 추가

--- Traplo/Traplo/LoginViewController.swift
+++ Traplo/Traplo/LoginViewController.swift
@@ -1,9 +1,10 @@
 //
-//  LoginViewController.swift
+//  LogInViewController.swift
 //  Traplo
 //
-//  Created by 김상현 on 2021/08/10.
+//  Created by 김상현 on 2021/08/11.
 //
+
 
 import Foundation
 import UIKit
@@ -12,15 +13,50 @@ import KakaoSDKUser
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet var superView: UIView!
+    @IBOutlet weak var backGroundBtn: UIButton!
+    @IBOutlet weak var btnKakaoLogIn: UIButton!
+    @IBOutlet weak var btnGoogleLogIn: UIButton!
+    @IBOutlet weak var btnNaverLogIn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
 
+        backGroundBtn.layer.cornerRadius = 40
+        
+        btnKakaoLogIn.layer.borderColor = UIColor.white.cgColor
+        btnKakaoLogIn.layer.borderWidth = 2
+        btnKakaoLogIn.layer.cornerRadius = 20
+        
+        btnGoogleLogIn.layer.borderColor = UIColor.white.cgColor
+        btnGoogleLogIn.layer.borderWidth = 2
+        btnGoogleLogIn.layer.cornerRadius = 20
+        
+        btnNaverLogIn.layer.borderColor = UIColor.white.cgColor
+        btnNaverLogIn.layer.borderWidth = 2
+        btnNaverLogIn.layer.cornerRadius = 20
         
         
-        
+    }
+    // 카카오로 이용하기 버튼 함수
+    @IBAction func onKakaoLoginByAppTouched(_ sender: Any) {
+     // 카카오톡 설치 여부 확인
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+        // 카카오톡 로그인. api 호출 결과를 클로저로 전달.
+        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+            if let error = error {
+                // 예외 처리 (로그인 취소 등)
+                print(error)
+            }
+            else {
+                print("loginWithKakaoTalk() success.")
+               // do something
+                _ = oauthToken
+               // 어세스토큰
+               let accessToken = oauthToken?.accessToken
+            }
+        }
+      }
     }
     
 }
--- Traplo/Traplo/MainViewController.swift
+++ Traplo/Traplo/MainViewController.swift
@@ -9,50 +9,16 @@ import UIKit
 import KakaoSDKUser
 
 class MainViewController: UIViewController {
-
-    @IBOutlet weak var backGroundBtn: UIButton!
-    @IBOutlet weak var btnSignIn: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        backGroundBtn.layer.cornerRadius = 40
-        btnSignIn.layer.borderColor = UIColor.white.cgColor
-        btnSignIn.layer.borderWidth = 2
-        btnSignIn.layer.cornerRadius = 20
+         }
 
-       
-
-    }
-    
-//    @IBAction func pushBtnSignIn(_ sender: Any) {
-//        let vcName = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
-//        vcName?.modalPresentationStyle = .overFullScreen
-//        vcName?.modalPresentationStyle = .overCurrentContext
-//        self.present(vcName!, animated: true, completion: nil)
-//    }
-
-    
-    // 카카오로 이용하기 버튼 함수
-    @IBAction func onKakaoLoginByAppTouched(_ sender: Any) {
-     // 카카오톡 설치 여부 확인
-        if (UserApi.isKakaoTalkLoginAvailable()) {
-        // 카카오톡 로그인. api 호출 결과를 클로저로 전달.
-        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
-            if let error = error {
-                // 예외 처리 (로그인 취소 등)
-                print(error)
-            }
-            else {
-                print("loginWithKakaoTalk() success.")
-               // do something
-                _ = oauthToken
-               // 어세스토큰
-               let accessToken = oauthToken?.accessToken
-            }
-        }
-      }
+    override func viewDidAppear(_ animated: Bool) {
+        let vcName = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
+        vcName?.modalPresentationStyle = .overFullScreen
+        vcName?.modalPresentationStyle = .overCurrentContext
+        self.present(vcName!, animated: true, completion: nil)
     }
     
 }

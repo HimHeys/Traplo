4fb7a82dce2f78152437a7c2023d005028e46290
KimSangHyun <kshy1019@naver.com>
2021-08-11 19:35:22 +0900

    kakaoLogin 구현

--- Traplo/Traplo/LoginViewController.swift
+++ Traplo/Traplo/LoginViewController.swift
@@ -22,24 +22,5 @@ class LoginViewController: UIViewController {
         
         
     }
-    @IBAction func onKakaoLoginByAppTouched(_ sender: Any) {
-     // 카카오톡 설치 여부 확인
-        if (UserApi.isKakaoTalkLoginAvailable()) {
-        // 카카오톡 로그인. api 호출 결과를 클로저로 전달.
-        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
-            if let error = error {
-                // 예외 처리 (로그인 취소 등)
-                print(error)
-            }
-            else {
-                print("loginWithKakaoTalk() success.")
-               // do something
-                _ = oauthToken
-               // 어세스토큰
-               let accessToken = oauthToken?.accessToken
-            }
-        }
-      }
-    }
     
 }
--- Traplo/Traplo/MainViewController.swift
+++ Traplo/Traplo/MainViewController.swift
@@ -6,11 +6,11 @@
 //
 
 import UIKit
+import KakaoSDKUser
 
 class MainViewController: UIViewController {
 
     @IBOutlet weak var btnSignIn: UIButton!
-    @IBOutlet weak var btnSignUp: UIButton!
     
     
     override func viewDidLoad() {
@@ -20,17 +20,35 @@ class MainViewController: UIViewController {
         btnSignIn.layer.borderWidth = 2
         btnSignIn.layer.cornerRadius = 10
 
-        btnSignUp.layer.borderColor = UIColor.white.cgColor
-        btnSignUp.layer.borderWidth = 2
-        btnSignUp.layer.cornerRadius = 10
+       
 
     }
     
-    @IBAction func pushBtnSignIn(_ sender: Any) {
-        let vcName = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
-        vcName?.modalPresentationStyle = .overFullScreen
-        vcName?.modalPresentationStyle = .overCurrentContext
-        self.present(vcName!, animated: true, completion: nil)
+//    @IBAction func pushBtnSignIn(_ sender: Any) {
+//        let vcName = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
+//        vcName?.modalPresentationStyle = .overFullScreen
+//        vcName?.modalPresentationStyle = .overCurrentContext
+//        self.present(vcName!, animated: true, completion: nil)
+//    }
+//
+    @IBAction func onKakaoLoginByAppTouched(_ sender: Any) {
+     // 카카오톡 설치 여부 확인
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+        // 카카오톡 로그인. api 호출 결과를 클로저로 전달.
+        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+            if let error = error {
+                // 예외 처리 (로그인 취소 등)
+                print(error)
+            }
+            else {
+                print("loginWithKakaoTalk() success.")
+               // do something
+                _ = oauthToken
+               // 어세스토큰
+               let accessToken = oauthToken?.accessToken
+            }
+        }
+      }
     }
     
 }

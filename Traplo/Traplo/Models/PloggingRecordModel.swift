95ba06ca0c07ceb9b200ec2769f43c2014308a80
KimSangHyun <kshy1019@naver.com>
2021-08-25 16:45:57 +0900

    PloggingRecordModelManager 추가
    여러개 존재하는 PloggingRecordModel 객체를 관리합니다.

--- Traplo/Traplo/Models/PloggingRecordModel.swift
+++ Traplo/Traplo/Models/PloggingRecordModel.swift
@@ -8,19 +8,29 @@
 import Foundation
 
 struct PloggingRecordModel {
-    let date:date
-    let distance:Float
-    let time:Float
-    let start:String
-    let end:String
-    init(date:date,distance:Float,time:Float,start:String,end:String) {
+    var modelId : Int // model 아이디
+    
+    var owner : String // 기록 작성자
+    
+    var date:date // 기록일
+    var distance:Float // 거리
+    var time:Float // 걸린 시간
+    var start:String // 시작 위치
+    var end:String // 종료 위치
+    mutating func update(date:date,distance:Float,time:Float,start:String,end:String) {
+        
         self.date = date
         self.distance = distance
         self.time = time
         self.start = start
         self.end = end
     }
+    // Equatable 로 '==' 연산자 임의로 정의.
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.modelId == rhs.modelId
+    }
 }
+
 struct date {
     let year:Int
     let month:Int
@@ -33,4 +43,22 @@ struct date {
     var date: String{
         return "\(year)+.+\(month)+.+\(day)"
     }
+}
+
+// 여러개 존재하는 PloggingRecordModel 객체를 관리합니다.
+class PloggingRecordModelManager {
+    
+    static let PloggingRecordModelShared = PloggingRecordModelManager()
+    
+    static var ploggingRecordModelLastId: Int = 0
+    
+    func createPloggingRecordModel(date:date,distance:Float,time:Float,start:String,end:String)->PloggingRecordModel{
+        let nextId = Self.ploggingRecordModelLastId + 1
+        Self.ploggingRecordModelLastId = nextId
+        
+       // let owner =  정의 해야함 !!
+        return PloggingRecordModel(modelId: nextId, owner: "String", date: date, distance: distance, time: time, start: start, end: end)
+    }
+    
+    
 }

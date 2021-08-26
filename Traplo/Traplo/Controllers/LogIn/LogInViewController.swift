0460cafce4d0d542a89eb5da066db169a60999fe
KimSangHyun <kshy1019@naver.com>
2021-08-26 12:56:52 +0900

    login서버연동-2

--- Traplo/Traplo/Controllers/LogIn/LogInViewController.swift
+++ Traplo/Traplo/Controllers/LogIn/LogInViewController.swift
@@ -32,36 +32,69 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loginInstance?.requestDeleteToken()
-
-    
-        backGroundBtn.layer.cornerRadius = 40
-        
-        btnKakaoLogIn.layer.borderColor = UIColor.white.cgColor
-        btnKakaoLogIn.layer.borderWidth = 2
-        btnKakaoLogIn.layer.cornerRadius = 20
-        
-        btnGoogleLogIn.layer.borderColor = UIColor.white.cgColor
-        btnGoogleLogIn.layer.borderWidth = 2
-        btnGoogleLogIn.layer.cornerRadius = 20
-        
-        btnNaverLogIn.layer.borderColor = UIColor.white.cgColor
-        btnNaverLogIn.layer.borderWidth = 2
-        btnNaverLogIn.layer.cornerRadius = 20
-        
+        setUI()
+        loginInstance?.resetToken()
     
     }
+    func setUI(){
+        
+            backGroundBtn.layer.cornerRadius = 40
+            
+            btnKakaoLogIn.layer.borderColor = UIColor.white.cgColor
+            btnKakaoLogIn.layer.borderWidth = 2
+            btnKakaoLogIn.layer.cornerRadius = 20
+            
+            btnGoogleLogIn.layer.borderColor = UIColor.white.cgColor
+            btnGoogleLogIn.layer.borderWidth = 2
+            btnGoogleLogIn.layer.cornerRadius = 20
+            
+            btnNaverLogIn.layer.borderColor = UIColor.white.cgColor
+            btnNaverLogIn.layer.borderWidth = 2
+            btnNaverLogIn.layer.cornerRadius = 20
+        
+    }
     
-    // 임시 코드 !!
-    // -> 로그인 되어야 화면 전환 가능하게 구현해야한다!
-    override func viewDidAppear(_ animated: Bool) {
+    func logInCompleted()  {
         
-//        let sb = UIStoryboard(name: "Activity", bundle: nil)
-//        let vc = sb.instantiateViewController(identifier: "RecommendActivityViewController") as! RecommendActivityViewController
-//        vc.modalPresentationStyle = .fullScreen
-//        vc.modalTransitionStyle = .crossDissolve
-//        self.present(vc, animated: true, completion: nil)
+        let sb = UIStoryboard(name: "Activity", bundle: nil)
+        let vc = sb.instantiateViewController(identifier: "RecommendActivityViewController") as! RecommendActivityViewController
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .crossDissolve
+        self.present(vc, animated: true, completion: nil)
         
+    }
+    // 카카오,네이버 서버 통신
+    func AFRequestByToken(authorization:String,com:String){
+        
+        let urlStr = "http://3.35.202.118:8080/api/v1/members/"+com
+        let url = URL(string: urlStr)!
+
+        let req = AF.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: ["Authorization": authorization])
+               
+        req.responseJSON { response in
+            print(response.result)
+//          guard let result = response.result as? [String: Any] else { return }
+//          guard let object = result["response"] as? [String: Any] else { return }
+//          guard let name = object["name"] as? String else { return }
+//          guard let email = object["email"] as? String else { return }
+//          print(name+" "+email)
+        }
+    }
+    // 구글 토큰 서버 통신
+    func AFRequestByTokenGoogle(authorization:String){
+        let urlStr = "http://3.35.202.118:8080/api/v1/members/google"
+        let url = URL(string: urlStr)!
+
+        let req = AF.request(url, method: .post, parameters: nil, encoding: URLEncoding.default, headers: ["Authorization": authorization])
+               
+        req.responseJSON { response in
+            print(response.result)
+//          guard let result = response.result as? [String: Any] else { return }
+//          guard let object = result["response"] as? [String: Any] else { return }
+//          guard let name = object["name"] as? String else { return }
+//          guard let email = object["email"] as? String else { return }
+//          print(name+" "+email)
+        }
     }
 
     
@@ -69,12 +102,19 @@ class LoginViewController: UIViewController {
     @IBAction func signIn(sender: Any) {
         GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: self) { user, error in
         guard error == nil else { return }
-
-        // If sign in succeeded, display the app's main content View.
-      }
+        guard let user = user else { return }
+            
+        user.authentication.do { authentication, error in
+            guard error == nil else { return }
+            guard let authentication = authentication else { return }
+            
+            guard let authorization = authentication.idToken else {return}
+            self.AFRequestByTokenGoogle(authorization: authorization)
+            
+            }
+        }
     }
    
-
     // 카카오로 이용하기 버튼 함수
     @IBAction func onKakaoLoginByAppTouched(_ sender: Any) {
      // 카카오톡 설치 여부 확인
@@ -86,30 +126,10 @@ class LoginViewController: UIViewController {
                 print(error)
             }
             else {
-                print("loginWithKakaoTalk() success.")
-               // do something
-                _ = oauthToken
-               // 어세스토큰
-               let accessToken = oauthToken?.accessToken
-                
-                
-                let urlStr = "http://3.35.202.118:8080/api/v1/members/oauthKakao"
-                let url = URL(string: urlStr)!
+               guard let accessToken = oauthToken?.accessToken else {return}
+               let authorization = "\(accessToken)"
 
-                let authorization = "\(accessToken)"
-
-                let req = AF.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: ["authorization": authorization])
-                  
-
-                req.responseJSON { response in
-                  print(response)
-          //        guard let result = response.result as? [String: Any] else { return }
-          //        guard let object = result["response"] as? [String: Any] else { return }
-          //        guard let name = object["name"] as? String else { return }
-          //        guard let email = object["email"] as? String else { return }
-          //
-          //        print(name+" "+email)
-                }
+                self.AFRequestByToken(authorization: authorization,com: "oauthKakao")
             }
         }
       }
@@ -128,37 +148,17 @@ class LoginViewController: UIViewController {
         
         return
       }
-      
-      guard let tokenType = loginInstance?.tokenType else { return }
-      guard let accessToken = loginInstance?.accessToken else { return }
-      let urlStr = "http://3.35.202.118:8080/api/v1/members/oauthNaver"
-      let url = URL(string: urlStr)!
+        guard let accessToken = loginInstance?.accessToken else { return }
 
-      let authorization = "\(accessToken)"
-
-      let req = AF.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: ["authorization": authorization])
+        let authorization = "\(accessToken)"
+        AFRequestByToken(authorization: authorization,com: "oauthNaver")
         
-
-      req.responseJSON { response in
-        print(response)
-//          guard let result = response.result as? [String: Any] else { return }
-//        guard let object = result["response"] as? [String: Any] else { return }
-//        guard let name = object["name"] as? String else { return }
-//        guard let email = object["email"] as? String else { return }
-//
-//        print(name+" "+email)
-      }
     }
 }
 
 // 네이버
 extension LoginViewController: NaverThirdPartyLoginConnectionDelegate {
     
-
-  func oauth20ConnectionDidOpenInAppBrowser(forOAuth request: URLRequest!) {
-  }
-    
-
   // 로그인에 성공했을 경우 호출
   func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
     print("[Success] : Success Naver Login")
@@ -179,4 +179,5 @@ extension LoginViewController: NaverThirdPartyLoginConnectionDelegate {
   func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
     print("[Error] :", error.localizedDescription)
   }
+    
 }

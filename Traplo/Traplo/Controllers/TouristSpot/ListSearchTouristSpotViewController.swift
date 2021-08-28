a5d28bda4b09860b96fd1dd8641d04b9b6f2b61a
KimSangHyun <kshy1019@naver.com>
2021-08-28 15:53:37 +0900

    관광지 선택 페이지 init

---
+++ Traplo/Traplo/Controllers/TouristSpot/ListSearchTouristSpotViewController.swift
@@ -0,0 +1,29 @@
+//
+//  listSearchTouristSpotViewController.swift
+//  Traplo
+//
+//  Created by 김상현 on 2021/08/28.
+//
+
+import UIKit
+
+class ListSearchTouristSpotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

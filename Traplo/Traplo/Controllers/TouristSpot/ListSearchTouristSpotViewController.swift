d097006d5cd886b58f887a32f9c00b0a8748ce3a
KimSangHyun <kshy1019@naver.com>
2021-08-30 20:23:58 +0900

    UI작업 -1

--- Traplo/Traplo/Controllers/TouristSpot/ListSearchTouristSpotViewController.swift
+++ Traplo/Traplo/Controllers/TouristSpot/ListSearchTouristSpotViewController.swift
@@ -9,22 +9,37 @@ import UIKit
 
 // 관광지 검색한 후 -list 페이지
 class ListSearchTouristSpotViewController: UIViewController {
-
+    
+    //gradient
+    var gradientLayer: CAGradientLayer!
+    
+    //UIColor
+    let topDesignColor1 = UIColor(named: "Color2")?.cgColor
+    let topDesignColor2 = UIColor(named: "Color1")?.cgColor
+    @IBOutlet weak var topDesignView: UIView!
+    @IBOutlet weak var topDesignLayoutView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUI()
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUI() {
+        setTopGradationDesign()
     }
-    */
+    // 상단 그라데이션 디자인
+    func setTopGradationDesign() {
+        
+        self.gradientLayer = CAGradientLayer()
+        self.gradientLayer.frame = topDesignView.bounds
+        self.gradientLayer.colors = [topDesignColor1 as Any,topDesignColor2 as Any]
+        self.gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        self.gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        self.topDesignView.layer.addSublayer(self.gradientLayer)
 
+        self.topDesignView.bringSubviewToFront(topDesignLayoutView)
+    }
 }
--- Traplo/Traplo/Controllers/TouristSpot/SelectTouristSpotViewController.swift
+++ Traplo/Traplo/Controllers/TouristSpot/SelectTouristSpotViewController.swift
@@ -9,22 +9,16 @@ import UIKit
 
 // 관광지 선택 페이지
 class SelectTouristSpotViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUI()
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUI() {
+       
     }
-    */
-
+   
 }

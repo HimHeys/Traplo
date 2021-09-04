2e92580ce6c87602f51289986167e4d41bd608bb
KimSangHyun <kshy1019@naver.com>
2021-09-04 16:15:32 +0900

    tourspot UI작업 -4

--- Traplo/Traplo/Controllers/TouristSpot/MapSearchTouristSpotViewController.swift
+++ Traplo/Traplo/Controllers/TouristSpot/MapSearchTouristSpotViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 // 관광지 검색한 후 -map 페이지
 class MapSearchTouristSpotViewController: UIViewController {
 
+    let array = ["  문화 역사  ","  이색거리  ","  식당  ","  자연  ","  둘레길  "]
     
     //gradient
     var gradientLayer: CAGradientLayer!
@@ -22,6 +23,7 @@ class MapSearchTouristSpotViewController: UIViewController {
     @IBOutlet weak var topDesignView: UIView!
     @IBOutlet weak var topDesignLayoutView: UIView!
     @IBOutlet weak var toggleBtn: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     
     override func viewDidLoad() {
@@ -54,4 +56,50 @@ class MapSearchTouristSpotViewController: UIViewController {
         self.topDesignView.bringSubviewToFront(topDesignLayoutView)
     }
    
+}
+extension MapSearchTouristSpotViewController:UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+     
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "searchKeyWord", for: indexPath) as? searchKeyWordCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        
+        cell.setUI(title: array[indexPath.row])
+        
+        return cell
+    }
+
+}
+
+extension MapSearchTouristSpotViewController:UICollectionViewDelegateFlowLayout{
+    
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+            //width 세팅에 사용됨
+            let maxSize = CGSize(width: 250, height: 250)
+            let heightOnFont = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+            //width
+            let collectionViewCellWidth = NSString(string: array[indexPath.row]).boundingRect(with: maxSize, options: heightOnFont, attributes: [.font: UIFont.systemFont(ofSize: 17)], context: nil)
+            //height
+            let collectionViewCellHeight = collectionView.bounds.height
+            return CGSize(width: collectionViewCellWidth.width, height: collectionViewCellHeight)
+        }
+    
+}
+
+class searchKeyWordCollectionViewCell : UICollectionViewCell {
+    @IBOutlet weak var keyword: UIButton!
+    
+    func setUI(title : String){
+        keyword.layer.cornerRadius = 12.5
+        keyword.setTitle(title, for: UIControl.State.normal)
+    }
+
 }
